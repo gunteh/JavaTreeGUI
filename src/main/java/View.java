6ab57f2fc6b676284ef39e72eef2fbc08a6c47e1
gunteh <gunter.herd2@gmail.com>
2024-04-25 03:12:48 -0700

    JavaFX Implementation - preliminary file upload V0.0.2

---
+++ src/main/java/View.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+// import /Users/gunterherd/NetBeansProjects/JavaTreeGUI/src/main/java/BstVisualizer.java
+//import JavaTreeGUI.src.main.java.BstVisualizer; 
+//import JavaTreeGUI.src.main.java.AvlVisualiser; 
+
+public class View {
+    @FXML
+    private Button avl;
+    @FXML
+    private Button bst;
+
+    @FXML
+    private void bstAction(){
+        bst.setOnAction(e-> setStage(new BstVisualizer()));
+    }
+    
+    @FXML
+    private void avlAction(){
+        avl.setOnAction(e-> setStage(new AvlVisualizer()));
+    }
+
+
+    private void setStage(BstVisualizer menu){
+        Stage menuStage = new Stage();
+        menu.start(menuStage);
+        menuStage.show();
+    }
+}
