1ef61c03fd302e64448704335283f72c685eb336
arcCreate <arckumar12@gmail.com>
2024-04-25 21:45:47 -0700

    AVL UI

--- src/main/java/com/mycompany/javatreegui/AVLController.java
+++ src/main/java/com/mycompany/javatreegui/AVLController.java
@@ -3,19 +3,15 @@ package com.mycompany.javatreegui;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class HomePageController {
-
+public class AVLController {
     @FXML
-    private void goToBST() throws IOException {
-        App.setRoot("binaryTreeUI", 1000, 800);
-    }
-    @FXML
-    private void goToAVL() throws IOException {
-        App.setRoot("avlTreeUI", 1000, 800);
-    }
-
+    private void goHome() throws IOException {
+        App.setRoot("homePageUI", 400, 400);
+    }   
+    
     public void handleMouseEntered(MouseEvent event) {
         Button button = (Button) event.getSource();
         button.setStyle("-fx-background-color: #bc6c25; -fx-font-weight: bold;");
@@ -24,5 +20,19 @@ public class HomePageController {
     public void handleMouseExited(MouseEvent event) {
         Button button = (Button) event.getSource();
         button.setStyle("-fx-background-color: #dda15e; -fx-font-weight: bold;");
+    }
+
+    //Implement
+    @FXML
+    private void insertValue(){
+
+    }
+    @FXML
+    private void deleteValue(){
+
+    }
+    @FXML
+    private void searchValue(){
+
     }
 }
